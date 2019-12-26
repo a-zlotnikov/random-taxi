46745068af5a713c8a518acde5f7eac41f7bcd8f
Анатолий Злотников <Zlotnikov@MacBook-Air-Anatolij.local>
2019-12-26 11:38:48 +0300

    Release

--- middleware/index.js
+++ middleware/index.js
@@ -1,13 +1,20 @@
 module.exports = function(app) {
     const express = require('express');
     const path = require('path');
-    const hbs = require('hbs');
+    const handlebars = require('express-handlebars');
 
     app.use(express.urlencoded({extended: true}));
     app.use(express.json());
 
     app.use(express.static(path.join(__dirname, "..", 'public')));
 
-    app.set("views", path.join(__dirname, '..', "views"));
-    app.set("view engine", "hbs");
+    app.set('views', path.join(__dirname, '../views'));
+    const hbs = handlebars.create( {
+        defaultLayout: './layout',
+        extname: 'hbs',
+        layoutsDir: path.join(__dirname, '../views'),
+        partialsDir: path.join(__dirname, '../views')
+    });
+    app.engine( 'hbs', hbs.engine );
+    app.set('view engine', 'hbs');
 };
