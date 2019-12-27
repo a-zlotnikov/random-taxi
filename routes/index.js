9f69eb7d1a9dab1cdb3b33b674249d2772a321b5
Анатолий Злотников <Zlotnikov@MacBook-Air-Anatolij.local>
2019-12-27 15:27:03 +0300

    MVP 1.0.0.0.0.0.2

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    let a = (Math.random() * (38 - 37 + 1) + 37).toFixed(14);
-    let b = (Math.random() * (56 - 55 + 1) + 55 ).toFixed(14);
+    let a = (Math.random() * (38 - 37.3) + 37.3).toFixed(14);
+    let b = (Math.random() * (56 - 55.5) + 55.5).toFixed(14);
     res.render("index", {a: a, b: b});
 });
 
