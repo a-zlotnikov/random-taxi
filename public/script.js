28bf541630dff3b9dc348bd34523b3f54e026971
Анатолий Злотников <Zlotnikov@MacBook-Air-Anatolij.local>
2019-12-27 12:13:19 +0300

    MVP

--- public/script.js
+++ public/script.js
@@ -5,37 +5,5 @@ function init() {
             center: [55.650625, 37.62708],
             zoom: 10,
             controls: []
-        }),
-
-        /*
-         * Макет кнопки должен отображать поле data.content
-         * и изменяться в зависимости от того, нажата ли кнопка или нет.
-         * Текущий размер (small, medium, large) рассчитывается исходя из значения опции maxWidth
-         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/control.Button.xml#param-parameters
-         */
-        ButtonLayout = ymaps.templateLayoutFactory.createClass([
-            '<div title="{{ data.title }}" class="my-button ',
-            '{% if state.size == "small" %}my-button_small{% endif %}',
-            '{% if state.size == "medium" %}my-button_medium{% endif %}',
-            '{% if state.size == "large" %}my-button_large{% endif %}',
-            '{% if state.selected %} my-button-selected{% endif %}">',
-            '<img class="my-button__img" src="{{ data.image }}" alt="{{ data.title }}">',
-            '<span class="my-button__text">{{ data.content }}</span>',
-            '</div>'
-        ].join('')),
-
-        button = new ymaps.control.Button({
-            data: {
-                image: 'img/get.png',
-            },
-            options: {
-                layout: ButtonLayout,
-                maxWidth: [300, 300, 300]
-            }
-        });
-
-    myMap.controls.add(button, {
-        right: 5,
-        top: 5
-    });
+        })
 }
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
+
+
 router.get('/', (req, res) => {
-    res.render("index");
+    let a = (Math.random() * (38 - 37 + 1) + 37).toFixed(14);
+    let b = (Math.random() * (56 - 55 + 1) + 55 ).toFixed(14);
+    res.render("index", {a: a, b: b});
 });
 
 module.exports = router;
